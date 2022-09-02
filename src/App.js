3dd1d7d989ecb9eb090bbf193ba81422d209c4a9
julyusmanurung <julyusmanurung@gmail.com>
2022-09-02 12:35:16 +0700

    create profile and portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Projects from "./Projects";
+import SocialProfiles from "./SocialProfiles";
+import profile from './assets/profile.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = { displayBio: false};
+    
+    toggleDisplayBio = () => {
+        this.setState({ displayBio : !this.state.displayBio })
+    }
+
+    constructor() {
+        super();
+        this.state = { displayBio: false};
+        console.log('Component this', this);
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+    
+    render() {
+        return (
+            <div>
+                <img src={profile} alt='julyus andreas' class='profile'/>
+                <h1>Hello World!</h1>
+                <p>My name is Julyus Andreas. I'm a informatics student gradute from Parahyangan Catholic University.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in Bekasi, and sometimes code sometimes game every day.</p>
+                            <p>My favorite language is JavaScript, and I think React.js is awesome.</p>
+                            <p>Besides coding, I also love food!</p>
+                            <button onClick={this.toggleDisplayBio}>Show less</button>
+                        </div>
+                    ) : 
+                    ( 
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read more</button>
+                        </div>
+                    )        
+                }
+                <hr/>
+                <Projects></Projects>
+                <hr/>
+                <SocialProfiles></SocialProfiles>
+            </div>
+        )
+    }
 }
 
 export default App;
+
+// class RegularClass {}
+// class ComponentClass extends Component {}
+
+// const RegularClassInstance = new RegularClass();
+// const ComponentClassInstace = new ComponentClass();
+
+// console.log('RegularClassInstance', RegularClassInstance);
+// console.log('ComponentClassInstace', ComponentClassInstace);
---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+import project1 from '../assets/project1.png'
+import project2 from '../assets/project2.png'
+import project3 from '../assets/project3.png'
+
+const PROJECTS = [
+    {
+        id: 1, 
+        title: 'Example React Application',
+        description: 'A React App that i built, involving JS and core web dev concepts!',
+        link: 'https://github.com/julyusmanurung/first-react-app',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'My API',
+        description: 'A REST API that i built form scratch with GET and POST requests!',
+        link: 'https://github.com/julyusmanurung/first-react-app',
+        image: project2
+    },
+    {
+        id: 3,
+        title: 'Operating Systems Final Project',
+        description: 'My unique final project for my unversity Operating Systems course.',
+        link: 'https://github.com/julyusmanurung/first-react-app',
+        image: project3
+    }
+];
+
+export default PROJECTS;
