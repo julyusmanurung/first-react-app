75264f3a7669e9739774c44eab57980d9defa8cc
julyusmanurung <julyusmanurung@gmail.com>
2022-09-02 15:19:17 +0700

    missing some details

--- src/SocialProfiles.js
+++ src/SocialProfiles.js
@@ -6,7 +6,8 @@ class SocialProfile extends Component {
         const {link, image} = this.props.socialProfile;
         return (
             <div style={{display: 'inline-block'}}>
-                <a href={link}><img src={image} alt='social-profile' style={{width: 35, height: 35, margin: 10}}/></a>
+                <a href={link}><img src={image} alt='social-profile' style={{width: 35, height: 35, margin: 10
+                }}/></a>
             </div>
         )
     }
