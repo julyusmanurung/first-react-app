74f5d5bdc1ba251a423b436fdbf6c3593665360a
julyusmanurung <julyusmanurung@gmail.com>
2022-09-02 15:47:48 +0700

    add title animation

--- src/Title.js
+++ src/Title.js
@@ -8,10 +8,11 @@ const TITLES = [
 ];
 
 class Title extends Component {
-    state = { titleIndex: 0};
+    state = { titleIndex: 0, fadeIn: true};
 
     componentDidMount() {
-        console.log('Title component has mounted');
+       this.timeout = setTimeout(() => this.setState({ fadeIn: false}), 2000);
+
         this.animateTitles();
     }
 
@@ -19,21 +20,22 @@ class Title extends Component {
     animateTitles = () => {
         this.titleInterval = setInterval(() => {
             const titleIndex = (this.state.titleIndex + 1) % TITLES.length;
-            this.setState({ titleIndex });
+            this.setState({ titleIndex, fadeIn: true });
+            this.timeout = setTimeout(() => this.setState({ fadeIn: false }), 2000);
         }, 4000);
-        console.log('this.titleInterval', this.titleInterval);
     }
     
     componentWillUnmount() {
-        console.log('Title component will unmount!');
         clearInterval(this.titleInterval);
+        clearTimeout(this.timeout);
     }
 
     render() {
-        const title = TITLES[this.state.titleIndex];
+        const { fadeIn, titleIndex } = this.state;
+        const title = TITLES[titleIndex];
 
         return (
-            <p>I am {title}</p>
+            <p className={fadeIn ? 'title-fade-in' : 'title-fade-out'}>I am {title}</p>
         )
     }
 }
